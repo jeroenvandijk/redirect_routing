02da8335539de67f78d67b6a0cd7118411882d00
Jeroen van Dijk <jvandijk@noxa.nl>
2009-02-17 17:59:49 +0100

    Plugin extended with extra parameters and compatible with newest rails version

--- lib/redirect_routing_controller.rb
+++ lib/redirect_routing_controller.rb
@@ -1,32 +1,46 @@
 class RedirectRoutingController < ActionController::Base
-	# Orginal redirect controller didn't support dynamic urls. Furthermore, it wasn't backwards compatible with rails versions < 2.0
-	# because of the use of the method extract_options!
   def redirect
-		options = extract_options(params[:args]) # extract_options is declared below since it is not incorporated in older versions of rails
-		options.delete(:conditions)
-    headers["Status"] = options.delete(:permanent) == true ? "301 Moved Permanently" : "302 Moved Temporarily"
+		options = params[:args].extract_options!
+		status = options.delete(:permanent) == true ? :moved_permanently : 302
+
+		# Forward params
+		keys_to_ignore = %W(args action controller permanent)
+		new_params = params.reject { |key, _| keys_to_ignore.include?(key) }
+		
+		# controller and/or action are defined use 
 		url_options = params[:args].first || options
 
 		# When url_options is a string we should apply pattern matching
 		if url_options.is_a?(String)
 			# Add found params to url by search for symbols in the url
-			params.each_pair { |key, value| url_options.gsub!(/:#{key.to_s}/, value.to_s) }
+			params.each_pair do |key, value| 
+				if url_options =~ /:#{key.to_s}/
+					url_options.gsub!(/:#{key.to_s}/, value.to_s)
+					# also ignore the declared params in the path
+					new_params.delete(key)
+				end
+			end
 			# Remove all left symbols in the string
 			url_options.gsub!(/\/:.*/, "")
-			
-		# When url_options is a hash we just add new params
+					
+			# forward params doesn't work with arrays
+			param_pairs = []
+			new_params.each_pair do |key, value| 
+				if value.is_a?(String)
+					param_pairs << "#{key}=#{value}"
+				elsif value.is_a?(Array)
+					value.each {|v| param_pairs << "#{key}[]=#{v}" }
+				elsif value.is_a?(Hash)
+					value.each_pair {|k,v| param_pairs << "#{key}[#{k}]=#{v}" }
+				end
+			end
+			url_options += "?" + param_pairs.join("&") unless param_pairs.empty?
+
 		elsif url_options.is_a?(Hash)
+			# Forward all other params
 			url_options.reverse_merge!(new_params)
 		end
 
-    redirect_to url_options
+    redirect_to url_options, :status => status
   end
-
-	private
-		# Method adopted from Rails 2.1.2
-		# Extracts options from a set of arguments. Removes and returns the last element in the array if it’s a hash, otherwise returns a blank hash. 
-		def extract_options(hash)
-			hash.last.is_a?(::Hash) ? hash.pop : {}
-		end
-		
 end
